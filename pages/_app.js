171631ffda50c4da59420008b9102276b73dd5e3
TihomirPet <tihomirpetrov@web.de>
2022-10-13 10:56:40 +0200

    install gsap bootstrap gsap

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import '../sass/style.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
