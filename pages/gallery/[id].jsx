8ad46cf8af1d2d0706abfc1c259ff5aeb7b56b79
TihomirPet <tihomirpetrov@web.de>
2022-10-13 11:04:28 +0200

    add slug page gallery

---
+++ pages/gallery/[id].jsx
@@ -0,0 +1,44 @@
+// import React from 'react';
+
+// export async function getStaticPaths() {
+//   const response = await fetch(`${window.location.origin}/art/`);
+
+//   const data = await response.json();
+
+//   const paths = data.map((element) => {
+//     return {
+//       params: { id: element.id.toString() },
+//     };
+//   });
+
+//   return{
+//     paths,
+//     fallback:false
+//   }
+// }
+
+
+
+// export async function getStaticProps(context) {
+
+// const id =context.params.id
+//  const response = await fetch(`${window.location.origin}/art/${id}`);
+// const data = await response.json();
+
+// return {
+//   props: {element:data}
+// };
+
+// }
+
+
+
+// export default function Details({ element }) {
+//   return (
+//     <div>
+//       <h2>{element.title}</h2>
+//       <img src={element.img} width='150' />
+//     </div>
+//   );
+// }
+
